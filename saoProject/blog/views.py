1e6628fe2def947383cf83d6c660f7649cc3faaf
zalayetha <zalayetha@gmail.com>
2022-08-02 14:27:39 +0700

    menyempurnakan halaman blog dengan slug dan querysets

--- saoProject/blog/views.py
+++ saoProject/blog/views.py
@@ -1,5 +1,42 @@
+from unicodedata import category
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 def index(request):
-     return render(request,'blog/index.html')
+     post = Post.objects.all()
+     categories = Post.objects.values('category').distinct()
+   
+     context ={
+          'title' : 'Blog',
+          'subTitle':'Welcome to Aincrad Blog Post',
+          'Post' : post,
+          'Category':categories,
+     }
+
+     return render(request,'blog/index.html',context)
+
+def categoryPost(request,categoryInput):
+     post = Post.objects.filter(category=categoryInput)
+     categories = Post.objects.values('category').distinct()
+   
+     context ={
+          'title' : 'Blog',
+          'subTitle':'Welcome to Aincrad Blog Post',
+          'Post' : post,
+          'Category':categories,
+     }
+
+     return render(request,'blog/sub/categories.html',context)
+
+def detailPost(request,slugInput):
+     post = Post.objects.filter(slug=slugInput)
+     categories = Post.objects.values('category').distinct()
+   
+     context ={
+          'title' : 'Blog',
+          'subTitle':'Welcome to Aincrad Blog Post',
+          'Post' : post,
+          'Category':categories,
+     }
+
+     return render(request,'blog/sub/detailPost.html',context)
