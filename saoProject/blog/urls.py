ae23e055c0d4b78fffe775432f66ed7046056266
zalayetha <zalayetha@gmail.com>
2022-08-04 12:28:25 +0700

    add contact

--- saoProject/blog/urls.py
+++ saoProject/blog/urls.py
@@ -2,7 +2,9 @@ from django import views
 from django.urls import path,re_path
 from . import views
 urlpatterns = [
-    path('',views.index),
-    re_path(r'^category/(?P<categoryInput>[\w-]+)/$',views.categoryPost),
-    re_path(r'^post/(?P<slugInput>[\w-]+)/$',views.detailPost),
+    path('',views.index,name='index'),
+    re_path(r'^category/(?P<categoryInput>[\w-]+)/$',views.categoryPost,name='category'),
+    re_path(r'^post/(?P<slugInput>[\w-]+)/$',views.detailPost,name='detail'),
 ]
+
+app_name='blog'
